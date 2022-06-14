4dc162bcff58fbbde27eb8183d8c52b8dde92ece
Marina Kravchenko <marishka-k87@yandex.ru>
2022-06-14 23:10:10 +0300

    добавлен Counter

--- src/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import { CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components";
 import BurgerIngredientStyle from "./burger-ingredient.module.css";
 
 const BurgerIngredient = ({ ingredient }) => {
   return (
     <li className={BurgerIngredientStyle.card} key={ingredient._id}>
+      <Counter count={1} size="default" />
       <img src={ingredient.image} alt={ingredient.name} className="ml-4 mr-4" />
       <div className={`mt-1 mb-1 ${BurgerIngredientStyle.prise_info}`}>
         <p className="mr-2 text text_type_digits-default">{ingredient.price}</p>
